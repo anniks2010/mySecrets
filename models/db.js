b2917717a94e45a8e3ca16e7cc9761050d033dc1
anniks2010 <anulember@hot.ee>
2020-12-28 20:14:41 +0200

    commit1

--- models/db.js
+++ models/db.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 
 mongoose.connect(`mongodb://${process.env.DB_SERVER}:27017/secretDB`,{useNewUrlParser:true,useUnifiedTopology:true});
 mongoose.set('useCreateIndex',true);
-require('./secrets');
+require('./users');
